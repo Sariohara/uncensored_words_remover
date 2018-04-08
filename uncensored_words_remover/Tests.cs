0454dc04281a02884e61bf848403f37b2d173cf3
dragko19 <janickihubert@gmail.com>
2018-04-08 12:28:36 +0200

    first stable

--- uncensored_words_remover/Tests.cs
+++ uncensored_words_remover/Tests.cs
@@ -1,9 +1,5 @@
 ﻿using System;
 using System.IO;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 
 class Tests
@@ -12,20 +8,38 @@ class Tests
     {
         MemoryStream MemStr = new MemoryStream();
         Error_messenger err = new Error_messenger(ref MemStr);
+        string words_input_path = "uncensored_words.txt"; //words input file
+        string book_input_path= "book.txt"; // book input file
+        string book_output_file = "censored_book.txt"; //book output file
 
         //constructor tests
         {
-            string file_path1 = "words_input_file";
-            string file_path2 = "book_input_file";
-            string file_path3 = "book_output_file";
-            Words_remover obj = new Words_remover(file_path1, file_path2, file_path3);
-            if (obj._uncensored_words_file_path != file_path1 || obj._book_file_path != file_path2 || obj._censored_book_file_path != file_path3)
+
+            Words_remover obj = new Words_remover(words_input_path, book_input_path, book_output_file);
+            if (obj._uncensored_words_file_path != words_input_path || obj._book_file_path != book_input_path || obj._censored_book_file_path != book_output_file)
                 err.Report_error("Error in constructor tests");
         }
 
+        //
+        {
+            Words_remover obj = new Words_remover(words_input_path, book_input_path, book_output_file);
+            try
+            {
+                obj.Censore_text();
+                foreach (string word in obj._uncensored_words)
+                    Console.WriteLine(word);
+
+                Console.WriteLine(obj._uncensored_words.Count);
+                Console.WriteLine(obj._uncensored_words_count);
+            }
+            catch (Exception ex)
+            {
+                err.Report_error("Unexpected exception in Censore_text() method tests: " + ex.Message);
+            }
+        }
+        
         err.End_of_tests(); 
         Console.ReadLine();
-
     }
 }
 
--- uncensored_words_remover/words_remover.cs
+++ uncensored_words_remover/words_remover.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.IO;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 
 class Words_remover
@@ -11,19 +8,102 @@ class Words_remover
     public string _uncensored_words_file_path { get; private set; }
     public string _book_file_path { get; private set; }
     public string _censored_book_file_path { get; private set; }
-    private List<string> _uncensored_words;
-    StreamReader StrRd;
+    public List<string> _uncensored_words { get; private set; }
+    public int _uncensored_words_count { get; private set; }
+    
 
     public Words_remover(string uncensored_words_file_path, string book_file_path, string censored_book_file_path)
     {
         _uncensored_words_file_path = uncensored_words_file_path;
         _book_file_path = book_file_path;
         _censored_book_file_path = censored_book_file_path;
+        _uncensored_words = new List<string>();
+        _uncensored_words_count = 0;
     }
 
-    public void Print()
+    public void Censore_text()
     {
-        Console.WriteLine(StrRd.ReadToEnd());
+        Load_uncensored_words();
+        try
+        {
+            StreamReader StrRd = new StreamReader(_book_file_path);
+            MemoryStream MemStr = new MemoryStream();
+            StreamWriter StrWr = new StreamWriter(MemStr);
+            string line;
+            while((line = StrRd.ReadLine()) != null)
+            {
+                Compare(ref line);
+                StrWr.WriteLine(line);
+            }
+            StrRd.Close();
+
+            FileStream fS = new FileStream(_censored_book_file_path, FileMode.OpenOrCreate);
+            fS.SetLength(0);
+            MemStr.Seek(0,SeekOrigin.Begin);
+            MemStr.CopyTo(fS);
+            fS.Close();            
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }    
+
     }
+
+    private void Load_uncensored_words()
+    {
+        try
+        {
+            StreamReader StrRd = new StreamReader(_uncensored_words_file_path);
+            string line;
+            while ((line = StrRd.ReadLine()) != null)
+            {
+                _uncensored_words.Add(line);
+            }
+            StrRd.Close();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    } 
+
+    private void Compare(ref string line)
+    {
+        string[] words = Get_words_from_line(ref line);
+        line = "";
+        bool control = false;
+        foreach(string word in words)
+        {
+            foreach(string uncensored_word in _uncensored_words)
+            {
+                if (word.ToLower() == uncensored_word.ToLower())
+                {
+                    control = true;
+                    _uncensored_words_count++;
+                    for (int i = 0; i < word.Length; i++)
+                    {
+                        line += "*";
+                    }
+                }
+            }
+            if (!control)
+            {
+                line += word;
+            }
+            line += " ";
+            control = false;
+        }
+    }
+
+    private string[] Get_words_from_line(ref string line)
+    {
+        string[] separators = new string[] { " ", "  ", "   ", "    ", "-" };
+        string[] words = line.Split(separators, StringSplitOptions.None);
+
+        return words;
+    }
+    
+    
 }
 
